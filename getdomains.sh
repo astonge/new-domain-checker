3f23d57be9d00a82466dbacd8147a786204102ba
Adrian <astonge@gmail.com>
2018-10-02 19:39:32 -0700

    getdomains.sh working

--- getdomains.sh
+++ getdomains.sh
@@ -2,5 +2,5 @@
 
 #https://whoisds.com//whois-database/newly-registered-domains/2018-10-01.zip/nrd
 
-today="$(date +"%Y-%m-%d")"
+today="$(date -v -1d +"%Y-%m-%d")"
 curl "https://whoisds.com//whois-database/newly-registered-domains/$today.zip/nrd" -o $today.zip
