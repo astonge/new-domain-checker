9715f188d96db0f9b710d0cb5ae7e12dfd82eb2c
Adrian St. Onge <astonge@gmail.com>
2018-10-06 17:45:09 -0700

    this code is a pile of shit

--- getip.py
+++ getip.py
@@ -2,6 +2,8 @@
 
 import socket
 import sys
+import sqlite3
+import os.path
 
 def getip(d):
   try:
@@ -11,6 +13,20 @@ def getip(d):
   except Exception:
     return "NULL"
 
-for line in sys.stdin:
-  domain = line.strip()
-  print("%s:%s" % (domain,getip(domain)))
+DB = "new_domains.db"
+if os.path.exists(DB):
+  db = sqlite3.connect(DB)
+  print("Connected to database..")
+  # for line in sys.stdin:
+  # domain = line.strip()
+  # print("%s:%s" % (domain,getip(domain)))
+  cursor = db.cursor()
+  cursor.execute('''SELECT domain FROM domains LIMIT 10''')
+  domain_list = cursor.fetchall()
+  for d in domain_list:
+      print("%s:%s" % (d[0],getip(d[0])))
+  db.close()
+else:
+  print("DATABASE NOT FOUND")
+  print("RUN ./importdomains FIRST")
+  exit(-1)
--- getsubs.py
+++ getsubs.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python
 
+import os.path
+import os
+import sys
 import sqlite3
+import docker
 
-db = sqlite3.connect('testdomain.db')
-print("Connected to database..")
-
-cursor = db.cursor()
-cursor.execute('''SELECT domain FROM domains''')
-domain_list = cursor.fetchall()
-for d in domain_list:
-    print(d)
-
-print("Closeing database connection..")
-db.close()
+docker_client = docker.from_env()
+ARGS = "-v -nodns -norecursion -noalts"
+CMDSTR = ""
+DB = "new_domains.db"
+if os.path.exists(DB):
+    db = sqlite3.connect(DB)
+    cursor = db.cursor()
+    cursor.execute('''SELECT domain FROM domains''')
+    domain_list = cursor.fetchall()
+    for d in domain_list:
+        cmd = "docker run -t amass-docker %s -d %s" % (ARGS, d[0])
+        print(cmd)
+    db.close()
+else:
+    print("error")
